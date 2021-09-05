200b60580bdfb4d096700995113d8359a0e604df
sim01 <miaks255@gmail.com>
2021-09-06 01:08:35 +0300

    Try to fix ninth homework

--- stage9/Program.cs
+++ stage9/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace stage9
 {
@@ -16,33 +17,29 @@ namespace stage9
                 $"Время на генерацию и расчёт среднего арифметического(mass10M - {averagemass10M}, mass100M - {averagemass100M}) составило {timer.ElapsedMilliseconds} мс");
 
             timer = Stopwatch.StartNew();
-            var countdown = new CountdownEvent(2);
-            double threadaveragemass10M = 0;
-            double threadaveragemass100M = 0;
-            ThreadPool.QueueUserWorkItem
-            (
-                new WaitCallback
-                (
-                    (_) =>
-                    {
-                        threadaveragemass10M = CalcAdaveragemass10M();
-                        countdown.Signal();
-                    }
-                )
+            var rnd = new Random();
+            var mass10M = new int[10000000];
+            long summass10m = 0;
+            Parallel.For<long>(0, mass10M.Length, () => 0, (j, loop, subtotal) =>
+                {
+                    mass10M[j] = rnd.Next(0, 9999);
+                    subtotal += mass10M[j];
+                    return subtotal;
+                },
+                (x) => Interlocked.Add(ref summass10m, x)
             );
-            ThreadPool.QueueUserWorkItem
-            (
-                new WaitCallback
-                (
-                    (_) =>
-                    {
-                        threadaveragemass100M = CalcAdaveragemass100M();
-                        countdown.Signal();
-                    }
-                )
-            );
-            
-            countdown.Wait();
+
+            double threadaveragemass10M = summass10m / mass10M.Length;
+            var mass100M = new int[100000000];
+            long summass100m = 0;
+            Parallel.For(0, mass100M.Length,
+                i =>
+                {
+                    mass100M[i] = rnd.Next(0, 9999);
+
+                    Interlocked.Add(ref summass100m, mass100M[i]);
+                });
+            double threadaveragemass100M = summass100m / mass100M.Length;
             timer.Stop();
             Console.WriteLine(
                 $"Время с потоками на генерацию и расчёт среднего арифметического(mass10M - {threadaveragemass10M}, mass100M - {threadaveragemass100M}) составило {timer.ElapsedMilliseconds} мс");
